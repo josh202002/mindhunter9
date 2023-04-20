159c2b51afa399322061f71cae8d5f5bd34dcc28
josh202002 <joshkjayan@pm.me>
2023-04-20 12:18:13 +0530

    eding page added

---
+++ script.js
@@ -0,0 +1,95 @@
+function pg1(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("acideg2.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg2(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("bicdie3.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg3(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("zijyou4.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg4(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("cfbdi5.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg5(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("oiplom6.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg6(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("bbauop7.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg7(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("azghij8.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg8(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("acideg2.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
